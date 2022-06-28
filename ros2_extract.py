f80f56083642c11746296d8e61407506123df49a
BerryC-VU <2672919@student.vu.nl>
2022-06-28 19:59:53 +0200

    Data extracted: node names and msgs

--- ros2_extract.py
+++ ros2_extract.py
@@ -1,31 +1,40 @@
-import graphviz
 from rosbags.rosbag2 import Reader
 from rosbags.serde import deserialize_cdr
 from graphviz import Digraph
-import rosbags
-from rosbags.rosbag2 import ReaderError
-import sys
-import sqlite3
-
-# # create reader instance and open for reading
-# with Reader('/home/ros/rosbag_2020_03_24') as reader:
-#     for connection, timestamp, rawdata in reader.messages(['/imu_raw/Imu']):
-#         msg = deserialize_cdr(rawdata, connection.msgtype)
-#         print(msg.header.frame_id)
+import pandas as pd
+# import sys
 
 # bagfolder = sys.argv[1]
-bagfolder = 'ros2/rosbag2_2022_06_02-08_49_23_0'
+bagfolder = 'turtlesim_test/test4'
 graph = Digraph()
+df = pd.DataFrame()
 
+with Reader(bagfolder) as reader: # 这个地方用的是folder name,读取的是folder里面的metadata.yaml文件
+    topics = reader.topics
+    print(list(topics))
 
-# with Reader(bagfolder) as reader: # 这个地方用的是folder name,读取的是folder里面的metadata.yaml文件
-#     topics = reader.topics
-#     print("-----------------------")
-#     connections = [x for x in reader.connections if x.topic == '/amcl_pose/in_world']
-#     for connection, timestamp, rawdata in reader.messages(connections=connections):
-#         msg = deserialize_cdr(rawdata,connection.msgtype)
-#         print(msg,'\n')
+    connections = [x for x in reader.connections if x.topic == '/rosout']
 
-# conn = sqlite3.connect(bagfolder+'/'+'rosbag2_2022_06_02-08_49_23_0.db3')
-# print("Opened database successfully")
+    if len(connections) != 0:
+        print(len(connections))
+        msgs = []
+        nodes = []
+        stamps = []
+        for connection, timestamp, rawdata in reader.messages(list(connections)):
+            msg = deserialize_cdr(rawdata, connection.msgtype)
+            # print(msg.stamp.sec)
+            stamps.append(msg.stamp.sec)
+            nodes.append(msg.name)
+            msgs.append(msg.msg)
+            # print(msg.topics)
+        if len(msgs) == 0:
+            print("NO message in '/rosout'")
+        else:
+            data = pd.DataFrame({'timestamp': stamps, 'node': nodes, 'msg': msgs})
+            df = pd.concat([df,data])
+            print(df)
+            df.to_csv(bagfolder + '/info.csv')
+    else:
+        print("THERE is no '/rosout'")
+
 
