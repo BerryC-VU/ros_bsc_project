8027c006d29fbc45956a76e2287ba4ceb00a1d28
BerryC-VU <2672919@student.vu.nl>
2022-06-29 16:04:22 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# python3 main.py ros_version path_to_file
+
+import ros1_extract
+import ros2_extract
+import sys
+
+
+def main():
+    ros_ver = sys.argv[1]
+    path_to_file = 'rosbag2_2022_05_25-1/rosbag2_2022_05_25-15_53_16'
+    if ros_ver == 'ros1':
+        # path_to_file = sys.argv[2]
+        ros1_extract.main(path_to_file)
+    elif ros_ver == 'ros2':
+        # path_to_file = sys.argv[2]
+        ros2_extract.main(path_to_file)
+    else:
+        print("ROS version does not exist, please enter either 'ros1' or 'ros2'.")
+        sys.exit()
+
+
+if __name__ == '__main__':
+    main()
