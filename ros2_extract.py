4174ed46e62b61897ecb4f3d72338543bd99cda0
BerryC-VU <2672919@student.vu.nl>
2022-07-08 20:45:03 +0200

    convert timestamp to unix time

--- ros2_extract.py
+++ ros2_extract.py
@@ -6,27 +6,31 @@ from graphviz import Digraph
 import pandas as pd
 import group_topic
 import sys
+from datetime import datetime
 
 
-def read_rosout(bagfolder, reader, conns):
+def read_rosout(reader, conns):
     msgs = []
     nodes = []
-    stamps = []
+    # stamps = []
+    times = []
     df = pd.DataFrame()
 
     for conn, timestamp, rawdata in reader.messages(list(conns)):
         msg = deserialize_cdr(rawdata, conn.msgtype)
-        stamps.append(msg.stamp.sec)
-        nodes.append(msg.name)
         msgs.append(msg.msg)
-        if "Subscribing to" in msg.msg:
-            print("TRUE")
+        # stamps.append(msg.stamp.sec)
+        times.append(datetime.fromtimestamp(msg.stamp.sec))
+        nodes.append(msg.name)
+        # if "Subscribing to" in msg.msg:
+        #     print("TRUE")
     if len(msgs) == 0:
         print("NO message in '/rosout'")
     else:
-        data = pd.DataFrame({'timestamp': stamps, 'node': nodes, 'msg': msgs})
-        df = pd.concat([df,data])
-        df.to_csv(bagfolder + '/info.csv')
+        # data = pd.DataFrame({'timestamp': stamps, 'time': times, 'node': nodes, 'msg': msgs})
+        data = pd.DataFrame({'time': times, 'node': nodes, 'msg': msgs})
+        df = pd.concat([df, data])
+    # print(df)
     return df
 
 
@@ -34,7 +38,6 @@ def generate_topics(graph, all_topics):
     for topic in all_topics:
         if topic not in graph:
             graph.node(topic, topic, {'shape': 'rectangle'})
-
     group_topic.main(graph, all_topics)
 
 
@@ -52,7 +55,7 @@ def generate_edges(graph, all_topics):
         graph.edge('/_ros2cli_rosbag2', topic)
 
 
-def create_graph(graph, topics, all_info):
+def create_graph(graph, topics):
     generate_topics(graph, topics)
 
     # generate_nodes(graph, all_info)
@@ -62,6 +65,7 @@ def create_graph(graph, topics, all_info):
 
     generate_edges(graph, topics)
 
+
 def main(bagfolder):
     with Reader(bagfolder) as reader:
 
@@ -70,15 +74,18 @@ def main(bagfolder):
         # check whether '/rosout' exist since it is the only node that log information
         connections = [x for x in reader.connections if x.topic == '/rosout']
 
-        if len(connections) != 0: # if there is information logged in '/rosout'
-            all_info = read_rosout(bagfolder, reader, connections)
-            # print(len(connections))
+        if len(connections) != 0:  # if there is information logged in '/rosout'
+            all_info = read_rosout(reader, connections)
+            all_info.to_csv(bagfolder + '/info.csv')
         else:
             print("THERE is no '/rosout' topic")
             sys.exit()
 
-        graph = Digraph(name='ros2_'+bagfolder, strict=True)
-        create_graph(graph, topics, all_info)
+        # graph = Digraph(name='ros2_'+bagfolder, strict=True)
+        graph = Digraph(directory=bagfolder+'/', name='ros2_extraction', strict=True)
+        create_graph(graph, topics)
 
         # view graph
-        graph.unflatten(stagger=5, fanout=True).view()
+        # graph.unflatten(stagger=5, fanout=True).view()
+
+        return [reader.start_time, reader.end_time]
