a7f6a3da4ede999334f9c3264e9fa5188e6f9162
BerryC-VU <2672919@student.vu.nl>
2022-08-04 04:28:53 +0200

    update: pass time-window as parameters

--- main.py
+++ main.py
@@ -12,53 +12,64 @@ def date_to_datetime(time):
 
 
 def main():
-    # ros_ver = sys.argv[1]
-    # # path_to_file = 'ros2/TUdelft/rosbag2_2022_07_01-09_25_19'
-    # # path_to_file = 'ros1/back_and_forth_small.bag'
-    # path_to_file =
+    path_to_file = sys.argv[1]
 
-    # if ros_ver == 'ros1':
-    #     # path_to_file = sys.argv[2]
-    #     ros1_extract.main(path_to_file, 'extract-graph')
-    # elif ros_ver == 'ros2':
+    start = sys.argv[2]
+    end = sys.argv[3]
 
-    # path_to_file = sys.argv[1]
-    # path_to_file = 'ros2/turtlesim_test/test8'
-    path_to_file = 'ros2/rosbag2_2022_06_02-08_49_23_0'
     with Reader(path_to_file) as reader:
         bag_start = datetime.fromtimestamp(reader.start_time // 1000000000)
         bag_end = datetime.fromtimestamp(reader.end_time // 1000000000)
-    print("The bagfile STARTS at：", bag_start, " and ENDS at: ", bag_end)
+        print("The bagfile STARTS at：", bag_start, " and ENDS at: ", bag_end)
 
-    while True:
-        option = input("Do you want to extract the graph from a specific time window? (y/n)  ")
-        if option != 'y' and option != 'n':
-            continue
-        break
-    if option == 'y':
-        while True:
-            start_t = input("Enter the preferred START-time: (yyyy-mm-dd xx:xx:xx) ")
-            if date_to_datetime(start_t) < bag_start or date_to_datetime(start_t) > bag_end:
-                print("Please input a valid start time.")
-                continue
-            else: break
-        while True:
-            end_t = input("Enter the preferred END-time: (yyyy-mm-dd xx:xx:xx) ")
-            if date_to_datetime(end_t) > bag_end or date_to_datetime(end_t) < bag_start:
-                if date_to_datetime(end_t) < date_to_datetime(start_t):
-                    print("Please input a valid end time.")
-                    continue
-            else: break
-        print("Start extracting the graph from ", start_t, " to ", end_t)
-        ros2_extract.main(path_to_file,
-                          date_to_datetime(start_t),
-                          date_to_datetime(end_t))
+    if start == 'start':
+        start_t = bag_start
     else:
-        print("Start extracting the graph from ", bag_start, " to ", bag_end)
-        ros2_extract.main(path_to_file,
-                          bag_start,
-                          bag_end)
+        if date_to_datetime(start) < bag_start or date_to_datetime(start) > bag_end:
+            print("Please input a valid start time.")
+            sys.exit()
+        else:
+            start_t = date_to_datetime(start)
 
+    if end == 'end':
+        end_t = bag_end
+    else:
+        if date_to_datetime(end) > bag_end or date_to_datetime(end) < bag_start:
+            print("Please input a valid end time.")
+            sys.exit()
+        else:
+            end_t = date_to_datetime(end)
+
+    ros2_extract.main(path_to_file, start_t, end_t)
+
+
+    #
+    # while True:
+    #     option = input("Do you want to extract the graph from a specific time window? (y/n)  ")
+    #     if option != 'y' and option != 'n':
+    #         continue
+    #     break
+    # if option == 'y':
+    #     while True:
+    #         start_t = input("Enter the preferred START-time: (yyyy-mm-dd xx:xx:xx) ")
+    #         if date_to_datetime(start_t) < bag_start or date_to_datetime(start_t) > bag_end:
+    #             print("Please input a valid start time.")
+    #             continue
+    #         else: break
+    #     while True:
+    #         end_t = input("Enter the preferred END-time: (yyyy-mm-dd xx:xx:xx) ")
+    #         if date_to_datetime(end_t) > bag_end or date_to_datetime(end_t) < bag_start:
+    #             if date_to_datetime(end_t) < date_to_datetime(start_t):
+    #                 print("Please input a valid end time.")
+    #                 continue
+    #         else: break
+    #     print("Start extracting the graph from ", start_t, " to ", end_t)
+    #     ros2_extract.main(path_to_file,
+    #                       date_to_datetime(start_t),
+    #                       date_to_datetime(end_t))
+    # else:
+    #     print("Start extracting the graph from ", bag_start, " to ", bag_end)
+    #     ros2_extract.main(path_to_file, bag_start, bag_end)
     # else:
     #     print("ROS version does not exist, please enter either 'ros1' or 'ros2'.")
     #     sys.exit()
--- ros2_extract.py
+++ ros2_extract.py
@@ -4,7 +4,6 @@ from graphviz import Digraph
 import pandas as pd
 import group_topic
 import os
-import sys
 from datetime import datetime
 
 
@@ -15,26 +14,11 @@ def get_file_path(bagfolder, topic):
 def get_msg_and_info(reader, connections, topic):
     stamps = []
     df = pd.DataFrame()
-    # if topic == '/rosout':
-    #     msgs = []
-    #     date_times = []
-    #     nodes = []
-    #     for conn, timestamp, rawdata in reader.messages(list(connections)):
-    #         msg = deserialize_cdr(rawdata, conn.msgtype)
-    #         msgs.append(msg.msg)
-    #         date_times.append(datetime.fromtimestamp(msg.stamp.sec))
-    #         nodes.append(msg.name)
-    #         stamps.append(timestamp * (10 ** -9))
-    #
-    #     data = pd.DataFrame({'Stamps': stamps, 'Date-Time': date_times, 'Node': nodes, 'Msg': msgs})
-    #
-    # else:
     for conn, timestamp, rawdata in reader.messages(list(connections)):
-        # print(type(timestamp))
-
         stamps.append(timestamp * (10 ** -9))
     data = pd.DataFrame({'Stamps': stamps})
     df = pd.concat([df, data], ignore_index=True)
+
     return df
 
 
@@ -50,6 +34,7 @@ def generate_topics(bagfolder, graph, topics):
                 graph.node(topic, topic, {'shape': 'rectangle'}, xlabel=(str(med_freq)+'Hz'))
             else:
                 graph.node(topic, topic, {'shape': 'rectangle'}, xlabel=(str(med_freq)))
+            # graph.node(topic, topic, {'shape': 'rectangle'})
     group_topic.main(graph, topics)
 
 
@@ -77,9 +62,6 @@ def create_graph(bagfolder, graph, topics):
     generate_topics(bagfolder, graph, topics)
     # generate_nodes(graph, all_info)
 
-    # add fixed nodes
-    graph.node('/_ros2cli_rosbag2', '/_ros2cli_rosbag2')
-
     generate_edges(graph, topics)
 
 
@@ -90,42 +72,51 @@ def get_freq(stamps):
     return med_freq
 
 
+def get_mean_freq(stamps):
+    n_messages = len(stamps)
+    total_time = stamps[len(stamps)-1] - stamps[0]
+    if total_time == 0.0:
+        mean_freq = float('nan')
+    else:
+        mean_freq = round((float(n_messages) / total_time), 2)
+    return mean_freq
+
+
 def main(bagfolder, start_t, end_t):
+    graph = Digraph(directory=bagfolder+'/', name='ros2_extraction', strict=True)
+    graph.graph_attr["rankdir"] = "LR"
+    # add fixed nodes
+    graph.node('/_ros2cli_rosbag2', '/_ros2cli_rosbag2')
+
     with Reader(bagfolder) as reader:
-        # if '/rosout' not in topics:
-        #     print("THERE is no '/rosout' topic")
-        #     sys.exit()
-        reader.start_time
         all_info = pd.DataFrame()
         for topic in list(reader.topics):
             connections = [x for x in reader.connections if x.topic == topic]
+
             topic_info = get_msg_and_info(reader, connections, topic)
-            data = pd.DataFrame({'topics': topic,
-                                 'start-time': topic_info['Stamps'].head(1).values,
-                                 'start-epoch': datetime.fromtimestamp(int(topic_info['Stamps'].head(1).values)),
-                                 'end-time': topic_info['Stamps'].tail(1).values,
-                                 'end-epoch': datetime.fromtimestamp(int(topic_info['Stamps'].tail(1).values)),
-                                 'frequency': get_freq(topic_info['Stamps'].tolist())})
-
-            all_info = pd.concat([all_info, data], ignore_index=True)
-
             file_path = get_file_path(bagfolder, topic)
+
             if os.path.exists(file_path):
                 os.remove(file_path)
-            topic_info.to_csv(file_path)
-        all_info.to_csv(bagfolder + '/' + 'all_info.csv')
+                topic_info.to_csv(file_path)
 
-        graph = Digraph(directory=bagfolder+'/', name='ros2_extraction', strict=True)
-        graph.graph_attr["rankdir"] = "LR"
+            if len(topic_info['Stamps'].head(1).values) != 0 and len(topic_info['Stamps'].tail(1).values) != 0:
+                data = pd.DataFrame({'topics': topic,
+                                     'start-time': topic_info['Stamps'].head(1).values,
+                                     'start-epoch': datetime.fromtimestamp(int(topic_info['Stamps'].head(1).values)),
+                                     'end-time': topic_info['Stamps'].tail(1).values,
+                                     'end-epoch': datetime.fromtimestamp(int(topic_info['Stamps'].tail(1).values)),
+                                     'med-frequency': get_freq(topic_info['Stamps'].tolist()),
+                                     'mean-frequency': get_mean_freq(topic_info['Stamps'].tolist())})
+
+                all_info = pd.concat([all_info, data], ignore_index=True)
+
+        all_info.to_csv(bagfolder + '/' + 'all_info.csv')
 
         topics = []
         for topic in list(reader.topics):
             topic_data = all_info.loc[all_info['topics'] == topic]
             if not topic_data.empty:
-                # if a topic starts at exactly the same time when the user-selected timewindow ends,
-                # it will still be presented on the graph
-                # if a topic ends at the same time when the user-selected timewindow starts,
-                # then the topic is not included in the graph
                 if datetime.fromtimestamp(int(topic_data['start-time'].values[0])) < end_t \
                         or datetime.fromtimestamp(int(topic_data['start-time'].values[0])) == end_t:
                     if datetime.fromtimestamp(int(topic_data['end-time'].values[0])) > start_t:
